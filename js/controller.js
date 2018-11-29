61d13ba9d636f8d586191cf9aab3972a423a4e56
Yrrieth <35847772+Yrrieth@users.noreply.github.com>
2018-11-30 00:25:56 +0100

    Ajout taille d'un sprite dans la classe et modif boucle
    Ajout de propriété dans la classe lié à la taille d'un sprite dans un spritesheet et possibilité de mettre des variables pour la taille visible d'un spritesheet dans le canvas, dans .drawImage() dans la boucle move()

--- js/controller.js
+++ js/controller.js
@@ -1,22 +1,22 @@
 class SpriteSheet {
-    constructor(source, numberRow, numberColumn) {
-        this.element = document.createElement("img");
+    constructor(source, numberRow, numberColumn, imageWidth, imageHeight) {
+        this.image = document.createElement("img");       
+
         this.source = source;
-        this.element.src = this.source;
+        this.image.src = this.source;
 
         this.numberRow = numberRow;
         this.numberColumn = numberColumn;
 
-        this.element = resizeSprite(this.element)
+        this.imageWidth = imageWidth;
+        this.imageHeight = imageHeight;
 
-        /*this.sizeSprite = sizeSprite(this.element, numberRow, numberColumn);
-        this.spriteWidth = this.sizeSprite[0];
-        this.spriteHeight = this.sizeSprite[1];*/
+        this.spriteWidth;
+        this.spriteHeight;
     }
 }
 
 function resizeSprite(image) {
-	console.log(image + image)
         if (image.width > canvas.width || image.height > canvas.height) {
 		    var coefficientDeReduction = Math.max(image.width / canvas.width, image.height / canvas.height);
 		    Math.round(image.width /= coefficientDeReduction);
@@ -26,12 +26,21 @@ function resizeSprite(image) {
         return image;
 }
 
-function sizeSprite(image, numberRow, numberColumn) {
-	var spriteWidth = image.width / numberRow;
-	var spriteHeight = image.height / numberColumn;
-	return [spriteWidth, spriteHeight];
-}
+/**
+ * @method spriteSize : calculate the size of 1 sprite from a spritesheet
+ * @param image : the object spritesheet
+ * @param numberRow : 
+ * @param numberColumn :
+ */
 
+function spriteSize(image, numberRow, numberColumn) {
+	var spriteWidth = image.imageWidth / numberColumn;
+	var spriteHeight = image.imageHeight / numberRow;
+	return {
+        spriteWidth : spriteWidth,
+        spriteHeight : spriteHeight
+    };
+}
 
 /**
  * @method on : call an event
@@ -61,7 +70,6 @@ function on(element, type, callback) {
 function newElement(element, parent, width, height, x, y, identifier, style) {
     var element = document.createElement(element);
     parent.appendChild(element);
-    element.style.position = "absolute";
     element.style.width = width + px;
     element.style.height = height + px;
     element.style.left = x + px;
@@ -73,12 +81,9 @@ function newElement(element, parent, width, height, x, y, identifier, style) {
     return element;
 }
 
-
-
-
 function move() {
     context.clearRect(0, 0, canvas.width, canvas.height);
-    context.drawImage(image, x, y, 57.5, 57.5, 0, 0, /*image.width*/ 50, /*image.height*/ 50);
+    context.drawImage(loadImage, x, y, load.spriteWidth, load.spriteHeight, 0, 0, /*canvas.width*/ 50, /*canvas.height*/ 50);
 
     if (ligne == nbLigne && colonne == nbColonne) {
         ligne = 1;
@@ -86,7 +91,6 @@ function move() {
         x = 0;
         y = 0;
     }
-    
     
     if (colonne < nbColonne) {
         colonne++;
@@ -100,21 +104,9 @@ function move() {
    
 }
 
-var tmp; 
-var t = 0; 
-
 function frameloop() {
-	/*Pour je ne sais quelle raison, apres la 1er boucle, 
-	 *la variable image devient du type object au type number 
-	 */ 
-
-	if (t == 1) { 
-		image = tmp; 
-	} 
 	move(); 
 
-	t = 1; 
-	tmp = image
     /*on(canvas, "resize", function() {
         console.log("w : "+window.innerWidth + " h : " +window.innerHeight)
         canvas.style.width = window.innerWidth;
